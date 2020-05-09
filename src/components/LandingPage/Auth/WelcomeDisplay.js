fb88d51e09ceffac9956a3bbfe2ecbfc18f47f6b
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-09 12:53:16 -0500

    Deciding on conditional render method 30 min

--- src/components/LandingPage/Auth/WelcomeDisplay.js
+++ src/components/LandingPage/Auth/WelcomeDisplay.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import WelcomeInitialContent from "./WelcomeInitialContent"
-import Login from "./Login"
-import Register from "./Register"
-import { Route } from 'react-router-dom'
+import React from "react";
+import WelcomeInitialContent from "./WelcomeInitialContent";
+import Login from "./Login";
+import Register from "./Register";
+import { Route } from "react-router-dom";
 
 export default function WelcomeDisplay() {
-    return (
-        <div className="welcome-container">
-          <WelcomeInitialContent />
-        </div>
-    )
+  let isLoggingIn = false;
+  let isRegistering = false;
+  return (
+    <div className="welcome-container">
+      {isLoggingIn ? (
+        <Login />
+      ) : isRegistering ? (
+        <Register />
+      ) : (
+        <WelcomeInitialContent />
+      )}
+    </div>
+  );
 }
