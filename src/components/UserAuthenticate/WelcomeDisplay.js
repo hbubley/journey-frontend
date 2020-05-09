3072b5cc1c1ae59f6eb7712a0dbd4af69b3854a7
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-08 22:29:19 -0500

    playing around with scss 1hr

--- src/components/UserAuthenticate/WelcomeDisplay.js
+++ src/components/UserAuthenticate/WelcomeDisplay.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function WelcomeDisplay() {
     return (
-        <div>
-        <img src="https://res.cloudinary.com/dum4u7sro/image/upload/v1588986601/icons_Journey_App_-_Desktop_hw04lx.png"></img>
-          <h1>Welcome</h1>  
+        <div className="welcome-container">
+          <h1>Welcome</h1> 
+          <a>Login</a> 
+          <a>Register</a>
         </div>
     )
 }
