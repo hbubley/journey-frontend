7f194073d86cdb87f93ae206e0342050b0918c1d
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-09 15:42:43 -0500

    Styled register, next need back button - 10 min

--- src/components/LandingPage/Auth/Register.js
+++ src/components/LandingPage/Auth/Register.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export default function Register() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <form className="register-container">
+      <h1>It's nice to meet you</h1>
+      <input
+        type="text"
+        className="form-control"
+        name="name"
+        placeholder="name"
+      />
+      <input
+        type="text"
+        className="form-control"
+        name="email"
+        placeholder="Email Address"
+      />
+      <input
+        type="password"
+        className="form-control"
+        name="password"
+        placeholder="Password"
+      />
+      <button className="btn btn-lg btn-primary btn-block" type="submit">
+        Register
+      </button>
+    </form>
+  );
 }
