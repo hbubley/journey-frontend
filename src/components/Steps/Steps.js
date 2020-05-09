ac64a3dcdff155b593073e0c2a4445895a3724b7
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-09 18:03:05 -0500

    Steps from context display 18:02

---
+++ src/components/Steps/Steps.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import StepContext from "../../context/user-steps/stepContext";
+export default function Steps() {
+  const stepContext = useContext(StepContext);
+
+  const { steps } = stepContext;
+  return (
+    <div>
+      {steps.map((step) => (
+        <h3>{step.good}</h3>
+      ))}
+    </div>
+  );
+}
