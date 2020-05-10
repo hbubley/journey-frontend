8249064b8b450e38a8d73065dc2da9782e6db3f8
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-09 19:03:02 -0500

    Done for today, attempting styling for homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import WelcomeDisplay from "./components/LandingPage/Auth/WelcomeDisplay";
 import About from "./components/LandingPage/About";
+import UserHome from './components/UserHome/UserHome'
 import JourneySequence from "./components/JourneySequence/JourneySequence";
 import StepState from "./context/user-steps/StepState";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -13,6 +14,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={WelcomeDisplay} />
             <Route path="/about" component={About} />
+            <Route path="/userhome" component={UserHome} />
             <Route path="/sequence" component={JourneySequence} />
           </Switch>
         </div>
