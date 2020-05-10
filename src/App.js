4199979ec543d2a56a084d9c4727c8701ecbdfc3
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-10 17:20:13 -0500

    Fixed naming convention and new structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/LandingPage/About";
 import UserDashboard from './components/UserDashboard/UserDashboard'
 import JourneySequence from "./components/JourneySequence/JourneySequence";
 import StepState from "./context/user-steps/StepState";
-import Steps from "./components/Steps/Steps"
+import Steps from "./components/UserSteps/UserSteps"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
--- src/components/JourneySequence/JourneySequence.js
+++ src/components/JourneySequence/JourneySequence.js
@@ -1,19 +1,14 @@
 import React, {useState} from 'react'
 import Breathe from './Breathe'
-import JourneyButtons from './JourneyButtons.js'
 import prompts from './prompts.json'
 import './Sequence.scss'
 
 export default function JourneySequence() {
-//    const [sequenceIndex, setSequenceIndex] = useState(0)
-//     switch (sequenceIndex) {
-//         case 0:
-//             return <Breathe prompts={prompts.breathe} />
-//     }
+   const [sequenceIndex, setSequenceIndex] = useState(0)
+  
     return (
         <div className="sequence-container">
            <Breathe prompts={prompts.breathe}/>
-           <JourneyButtons />
         </div>
     )
 }
