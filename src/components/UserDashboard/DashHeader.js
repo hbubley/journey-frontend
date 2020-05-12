e7acd32aebf5621561d8869d9998d7f630b1ee24
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-11 21:52:07 -0500

    Working user auth :)

--- src/components/UserDashboard/DashHeader.js
+++ src/components/UserDashboard/DashHeader.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import AuthContext from '../../context/auth/authContext'
 
 export default function DashHeader() {
+const authContext =  useContext(AuthContext)
+const { user } = authContext;
+if(user){
+    console.log(user)
     return (
         <div className='dash-header'>
-            <h1>We're so happy you're here, </h1>
+            <h1>We're so happy you're here, {user.name}</h1>
             <h2>Let us know when you're ready to begin your journey today</h2>
         </div>
     )
-}
+}else{ return <h1>loading</h1>}   }
