35a77fe60fb331383154cc3ca5c84bc63c6ed134
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-12 19:40:24 -0500

    Cleaning css

--- src/components/UserDashboard/DashHeader.js
+++ src/components/UserDashboard/DashHeader.js
@@ -8,8 +8,8 @@ if(user){
     console.log(user)
     return (
         <div className='dash-header'>
-            <h1>We're so happy you're here, {user.name}</h1>
-            <h2>Let us know when you're ready to begin your journey today</h2>
+            <h1>So happy to have you, {user.name}</h1>
+            <h2>Hit start to take a new step</h2>
         </div>
     )
 }else{ return <h1>loading</h1>}   }
