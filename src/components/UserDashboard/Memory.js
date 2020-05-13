b41cae9169920efe000992a22e9e27f4d9f72072
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-13 11:36:34 -0500

    App is MVP, need more styling

--- src/components/UserDashboard/Memory.js
+++ src/components/UserDashboard/Memory.js
@@ -8,21 +8,19 @@ export default function Memory() {
     let randomProud = "being here, in a place of growth"
     //ideally move this into it's own folder
     
-    if( steps && steps.length && !loading){
-        function randomNumber(min, max) {  
-            min = Math.ceil(min); 
-            max = Math.floor(max); 
-            return Math.floor(Math.random() * (max - min + 1)) + min; 
-        }  
-        let rn = randomNumber(0, steps.length)
+    // if( steps && steps.length && !loading){
+    //     function randomNumber(min, max) {  
+    //         min = Math.ceil(min); 
+    //         max = Math.floor(max); 
+    //         return Math.floor(Math.random() * (max - min + 1)) + min; 
+    //     }  
+    //     let rn = randomNumber(0, steps.length)
         
-        randomProud = steps[rn]
-    }
+    //     randomProud = steps[rn]
+    // }
     return (
         <div className='memory-container'>
-            {/* {steps !== null && !loading ? (
-            <h1>Remember when you were so proud of yourself for...{randomProud.proud_moment}</h1> */}
-            ) : <h1>Loading</h1>}
+           <h1>Be proud of yourself for being here, in a place of growth</h1>
         </div>
         
     )
