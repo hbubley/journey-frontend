3afe0067cd8e39dff266bb2bd454dd426994a6e2
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-13 15:01:11 -0500

    Scss fixes and such.

--- src/components/UserSteps/UserStep.js
+++ src/components/UserSteps/UserStep.js
@@ -1,17 +1,28 @@
 import React, { useContext, useEffect } from "react";
 import StepContext from "../../context/user-steps/stepContext";
-import './Steps.scss'
-export default function Step({step}) {
+import "./Steps.scss";
+export default function Step({ step }) {
   const stepContext = useContext(StepContext);
   const { steps, loading } = stepContext;
 
-  if(steps !== null && !loading){
-  return (
-    <div className='step-container'>
-        <h1>{step.date}</h1>
-        <h2>{step.feeling_now}</h2>
-      
-    </div>
-  );
-  }else{return <h1>Loading</h1>}
+  if (steps !== null && !loading) {
+    return (
+      <div className="step-container">
+        <h1>
+          <span>Feeling:</span> {step.feeling_now ? step.feeling_now : "Empty"}
+        </h1>
+        <h1>
+          <span>Thoughts:</span> {step.thoughts ? step.thoughts : "Empty"}
+        </h1>
+        <h1>
+          <span>Good thing:</span> {step.good_thing ? step.good_thing : "Empty"}
+        </h1>
+        <h1>
+          <span>Proud moment:</span> {step.feeling_now ? step.feeling_now : "Empty"}
+        </h1>
+      </div>
+    );
+  } else {
+    return <h1>Loading</h1>;
+  }
 }
